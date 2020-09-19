22a17d3c7e97fbefa016d9e58be3e6816a93f3c7
Mimina <yasmin.hamdy@gmail.com>
2020-09-19 11:38:54 +0200

    Locate nearest branch functionality added.

---
+++ branches.js
@@ -0,0 +1,42 @@
+var branches = [
+  {
+    name: 'Heliopolis Sporting Club',
+    hours: '5AM to 11:45PM',
+    coordinates: { lat: 30.088984, lng: 31.317309 },
+    address: '17 El-Marghany, Heliopolis, Cairo.',
+    number: '+20224170061',
+    distance: ""
+  },
+  {
+    name: 'Gezira Sporting Club',
+    hours: '9AM to 1AM',
+    coordinates: { lat: 30.052666, lng: 31.223163 },
+    address: '15 Saray El Gezirah, Zamalek, Cairo.',
+    number: '+20227356000',
+    distance: ""
+  },
+  {
+    name: 'El Shams Sporting Club',
+    hours: '6AM to 1AM',
+    coordinates: { lat: 30.116214, lng: 31.345354 },
+    address: 'Abd El-Hameed Badawi, El Nozha, Cairo.',
+    number: '+20221802460',
+    distance: ""
+  },
+  {
+    name: 'Al Ahly Club Nasr City',
+    hours: '6AM to 11PM',
+    coordinates: { lat: 30.070369, lng: 31.355282 },
+    address: 'Hassan Mamoon, Nasr City, Cairo.',
+    number: '+201111970758',
+    distance: ""
+  },
+  {
+    name: 'Shooting Club Dokki',
+    hours: '5AM to 12AM',
+    coordinates: { lat: 30.045612, lng: 31.204183 },
+    address: 'Dr Michel Bakhoum, Dokki, Giza.',
+    number: '+20233374559',
+    distance: ""
+  },
+];
--- index.js
+++ index.js
@@ -5,37 +5,22 @@
 */
 
 $(window).click(function (event) {
-  // anywhere on screen is clicked (except dropdown itself).
-  if (
-    !$(event.target).is('.selected-value') &&
-    $('.dropdown-items-ul').css('display') == 'block'
-  ) {
-    // hide dropdown if anywhere on screen is clicked (except dropdown itself).
-    $('.dropdown-items-ul').slideToggle('show-dropdown');
-  }
-
-  // if one of dropdown items is clicked, set is as the selected item.
-  if ($(event.target).parent().is('.dropdown-items-ul')) {
-    $('.selected-value').text(event.target.id);
-  }
 });
 
-function toggleDropdown() {
-  // show/hide dropdown when dropdown-div is clicked (called in HTML).
-  $('.dropdown-items-ul').slideToggle('show-dropdown');
-}
+
 
 /* ****************** Goggle Maps API ****************** */
+var currentLocation;
 var mapUrl = document.getElementById('map-iframe').src;
 
 function getCurrentLocation() {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition((position) => {
-      const pos = {
+      currentLocation = {
         lat: position.coords.latitude,
         lng: position.coords.longitude,
       };
-      console.log(pos);
+      console.log(currentLocation);
 
       document.getElementById('map-iframe').src = mapUrl.concat(
         '&z=14&ll=',
@@ -66,16 +51,105 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
   );
 }
 
-// let map, infoWindow;
+function locateNearestBranch() {
+  if (currentLocation == null) {
+    // get current location, if we don't have it.
+    getCurrentLocation();
+    return;
+  }
 
-// function initMap() {
-//   map = new google.maps.Map(document.getElementById('map'), {
-//     center: { lat: -34.397, lng: 150.644 },
-//     zoom: 6,
-//   });
-//   infoWindow = new google.maps.InfoWindow();
+  // set distances between current lcoation & branches.
+  setBranchesDistance(currentLocation, branches);
 
-//   // Try HTML5 geolocation.
+  let nearestBranch = getNearestBranch(branches);
 
-//   infoWindow.open(map);
-// }
+  clearBranchesDetailsDiv();
+  showBranchDetail(nearestBranch);
+}
+
+// Get Nearest Branch -------------------------------------------------------------
+function getNearestBranch(branches) {
+  let nearestBranch;
+  let i = branches[0].distance;
+
+  for (branch of branches) {
+    if (branch.distance <= i) {
+      nearestBranch = branch;
+    }
+  }
+  return nearestBranch;
+}
+
+// Clear Branches Details Div -----------------------------------------------------
+function clearBranchesDetailsDiv() {
+  $('.branches-details-div').html('');
+}
+
+// Show Branches Details Div ------------------------------------------------------
+
+function showAllBranches() {
+  clearBranchesDetailsDiv();
+
+  let i = 1;
+
+  for (branch of branches) {
+    showBranchDetail(branch);
+    if (i < branches.length) {
+      $('.branches-details-div').append('<hr>');
+    }
+    i++;
+  }
+}
+
+// Create Branch Details HTML ------------------------------------------------------
+
+function showBranchDetail(branch) {
+  if (branch.distance != '') {
+    $('.branches-details-div').append(`
+    <div class="branch-details-div">
+      <a class="branch-title">${branch.name}</a>
+      <a class="opening-hours">${branch.hours}</a>
+      <a class="distance">${Math.round(branch.distance * 10) / 10} km away from you</a>
+      <a class="address"><i class="fa fa-directions"></i> ${branch.address}</a>
+      <a class="phone"><i class="fa fa-phone-alt"></i> ${branch.number}</a>
+    </div>`);
+  } else {
+    $('.branches-details-div').append(`
+    <div class="branch-details-div">
+      <a class="branch-title">${branch.name}</a>
+      <a class="opening-hours">${branch.hours}</a>
+      <a class="address"><i class="fa fa-directions"></i> ${branch.address}</a>
+      <a class="phone"><i class="fa fa-phone-alt"></i> ${branch.number}</a>
+    </div>`);
+  }
+}
+
+// Get Distance (in km) between two coordinates -----------------------------------
+
+function haversine_distance(mk1, mk2) {
+  var R = 6371.071; // Radius of the Earth in kilometers
+  var rlat1 = mk1.lat * (Math.PI / 180); // Convert degrees to radians
+  var rlat2 = mk2.lat * (Math.PI / 180); // Convert degrees to radians
+  var difflat = rlat2 - rlat1; // Radian difference (latitudes)
+  var difflon = (mk2.lng - mk1.lng) * (Math.PI / 180); // Radian difference (longitudes)
+
+  var d =
+    2 *
+    R *
+    Math.asin(
+      Math.sqrt(
+        Math.sin(difflat / 2) * Math.sin(difflat / 2) +
+          Math.cos(rlat1) *
+            Math.cos(rlat2) *
+            Math.sin(difflon / 2) *
+            Math.sin(difflon / 2)
+      )
+    );
+  return d;
+}
+
+function setBranchesDistance(currentLoc, branchesJSON) {
+  for (branch of branchesJSON) {
+    branch.distance = haversine_distance(currentLoc, branch.coordinates);
+  }
+}
